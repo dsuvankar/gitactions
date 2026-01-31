ac35e5d4a3c0d4e182718c93b69040cb0dde8d4f
suvankar <iamsuvankar25@gmail.com>
2026-01-31 12:57:57 +0530

    check final

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get("/", (req, res) => {
-  return res.json({ msg: "Hello Server is Running, with CICD pipline" });
+  return res.json({
+    msg: "Hello Server is Running, with CICD pipline if it is working",
+  });
 });
 
 app.listen(PORT, () => {
