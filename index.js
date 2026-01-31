1486741be16d6cdd635a53d37a99cc8e658d55f5
suvankar <iamsuvankar25@gmail.com>
2026-01-31 12:46:41 +0530

    check if pipeline working

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get("/", (req, res) => {
-  return res.json({ msg: "Hello Server is Running" });
+  return res.json({ msg: "Hello Server is Running, with CICD pipline" });
 });
 
 app.listen(PORT, () => {
